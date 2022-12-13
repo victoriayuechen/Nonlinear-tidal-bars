cf2ebff559e555775894af1e45dd9ada6e028bf8
Bamischijf_275 <degrootjurgen39@gmail.com>
2022-12-13 10:53:24 +0100

    changed weak form, added friction and periodic BC

--- swe-solver/nonlinear_APVM_transient.jl
+++ swe-solver/nonlinear_APVM_transient.jl
@@ -9,7 +9,10 @@ using LinearAlgebra
 using LineSearches: BackTracking
 using GridapGmsh
 
-#Solves linear shallow water equations on a 2d plane
+#Solves nonlinear shallow water equations on a 2d plane
+#Makes use of the Anticipated Potential Vorticity Method (APVM) produced by McRae and Cotter: https://doi.org/10.1002/qj.2291 
+#Implementation in Gridap for this 2D domain also uses similar code produced by the Gridap GeoSciences github: https://github.com/gridapapps/GridapGeosciences.jl
+
 
 function uh(u₀,h₀,F₀,q₀,X,Y,dΩ)
     a((u,h,r,u2),(w,ϕ,ϕ2,w2)) = ∫(w⋅u + ϕ*h + w2⋅u2 + ϕ2*r)dΩ
@@ -53,8 +56,8 @@ function new_vtk_step(Ω,file,_cellfields)
               nsubcells=n)
 end
 
+#Perpendicular operator
 perp(u) = VectorValue(-u[2],u[1])
-coriolis(u) = 0.5*perp(u)
 
 function Gridap.get_free_dof_values(functions...)
     map(get_free_dof_values,functions)
@@ -62,27 +65,35 @@ function Gridap.get_free_dof_values(functions...)
 
 
 function Shallow_water_theta_newton(
-        order,degree,h₀,u₀,topography,
-        linear_solver::Gridap.Algebra.LinearSolver=Gridap.Algebra.BackslashSolver(),
-        sparse_matrix_type::Type{<:AbstractSparseMatrix}=SparseMatrixCSC{Float64,Int})
-    #Create model
+        order,degree,h₀,u₀,topography)
+
+
+    #Parameters
     B = 10
     L = 10
-    dx = 1
-    dy = 1
-    latitude = 52
+    partition = (50,50) #Split into X by X  cells
+    latitude = 52 #Latitude of the model being analysed
     η = 7.29e-5
-    f = 0.05#2*η*sin(latitude*(π/180))
+    f = 2*η*sin(latitude*(π/180))
+    cd = 0.025
     g = 9.81
+    periodic=true
+    T0 = 0.0
+    Tend = 10.0
+    dt = 0.1
+    τ = dt*0.5
 
     
-
+    #Save directory of output files
     dir = "swe-solver/output_linear_swe"
-    #Domain 
+    
+    #Make model
     domain = (0,B,0,L)
-    partition = (50,50)
-
-    model = CartesianDiscreteModel(domain,partition)
+    if periodic
+        model = CartesianDiscreteModel(domain,partition;isperiodic=(false,true))
+    else
+        model = CartesianDiscreteModel(domain,partition)
+    end
 
     #Make labels
     labels = get_face_labeling(model)
@@ -91,22 +102,30 @@ function Shallow_water_theta_newton(
     add_tag_from_tags!(labels,"right",[8])
     add_tag_from_tags!(labels,"top",[3,4,6])
     add_tag_from_tags!(labels,"inside",[9])
-    DC = ["right","left"]
-
     DC = ["left","right"]
 
+    #Make triangulations and boundaries
     Ω = Triangulation(model)
     dΩ = Measure(Ω,degree)
     dω = Measure(Ω,degree,ReferenceDomain())
-    Γ = BoundaryTriangulation(model,tags="boundary")
+    if periodic
+        Γ = BoundaryTriangulation(model,tags=DC)
+    else
+        Γ = BoundaryTriangulation(model,tags="boundary")
+    end
     nΓ = get_normal_vector(Γ)
     dΓ = Measure(Γ,degree)
-    τ=0.5
     
-
-    reffe_rt = ReferenceFE(raviart_thomas,Float64,order)
-    V = TestFESpace(model,reffe_rt;conformity=:HDiv,dirichlet_tags="boundary")
-    U = TransientTrialFESpace(V)
+    #Make FE spaces
+    if periodic
+        reffe_rt = ReferenceFE(raviart_thomas,Float64,order)
+        V = TestFESpace(model,reffe_rt;conformity=:HDiv,dirichlet_tags=DC)
+        U = TransientTrialFESpace(V)
+    else
+        reffe_rt = ReferenceFE(raviart_thomas,Float64,order)
+        V = TestFESpace(model,reffe_rt;conformity=:HDiv,dirichlet_tags="boundary")
+        U = TransientTrialFESpace(V)
+    end
 
     reffe_lgn = ReferenceFE(lagrangian,Float64,order)
     Q = TestFESpace(model,reffe_lgn;conformity=:L2)
@@ -122,70 +141,66 @@ function Shallow_water_theta_newton(
     Y = MultiFieldFESpace([V,Q,S,V])
     X = MultiFieldFESpace([U,P,R,U])
 
-    E = [0 -1; 1 0]
     #Create initial solutions
+    #u, velocity
     a1(u,v) = ∫(v⋅u)dΩ
     l1(v) = ∫(v⋅u₀)dΩ
     un = solve(AffineFEOperator(a1,l1,U,V))
-
+    #h, fluid depth
     a2(u,v) = ∫(v*u)dΩ
     l2(v) = ∫(v*h₀)dΩ
     hn = solve(AffineFEOperator(a2,l2,P,Q))
-
+    #b, topography
     a3(u,v) = ∫(v*u)dΩ
     l3(v) = ∫(v*topography)dΩ
     b = solve(AffineFEOperator(a3,l3,P,Q))
-
+    #Constant FE space with the coriolis parameter, possibly can be removed.
     a4(u,v)=∫(v*u)dΩ
     l4(v)=∫(v*f)dΩ
     fn=solve(AffineFEOperator(a4,l4,R,S))
 
-
+    #Build and compute initial potential vorticity
     q₀ = clone_fe_function(P,fn)
     compute_potential_vorticity!(q₀,H1MM,H1MMchol,dΩ,R,S,hn,un,fn)
 
-
-    unv,hnv = get_free_dof_values(un,hn)
+    #Build and compute initial mass flux
     F₀ = clone_fe_function(V,un)
     compute_mass_flux!(F₀,dΩ,V,RTMMchol,un*hn)
     
-    
+    #Solve whole coupled system with the initial values
     uhn = uh(un,hn,F₀,q₀,X,Y,dΩ)
     un,hn,q,F= uhn
 
-    #perp(u) = VectorValue(-u[2],u[1])
-
+    #Forcing function on u(t)
     forcfunc(t) = VectorValue(0.5*cos((1/10)*π*t),0.0)  
 
-    g = 9.81
-    res(t,(u,h,q,F),(w,ϕ,ϕ2,w2)) = ∫(∂t(u)⋅w + ((q-τ*(u⋅∇(q)))*(perp∘(F)))⋅w - (∇⋅(w))*(g*(h+b) + 0.5*(u⋅u)) + ∂t(h)*ϕ + w2⋅(F-u*h) + ϕ2*(q*h) +  (perp∘(∇(ϕ2)))⋅u - (ϕ2*fn) + ϕ*(∇⋅(F)))dΩ + ∫(g*(h+b)*(w⋅nΓ))dΓ
-    jac(t,(u,h,q,F),(du,dh,dq,dF),(w,ϕ,ϕ2,w2)) = ∫(((dq - τ*(u⋅∇(dq) + du⋅∇(q)))*(perp∘(F)))⋅w + ((q-τ*(u⋅∇(q)))*(perp∘(dF)))⋅w - (∇⋅(w))*(g*(dh) + (du⋅u)) + w2⋅(dF - du*h - dh*u) + ϕ2*(q*dh) + ϕ2*(dq*h) + du⋅(perp∘(∇(ϕ2))) + ϕ*(∇⋅(dF)))dΩ+ ∫(g*(dh)*(w⋅nΓ))dΓ
+    
+    res(t,(u,h,q,F),(w,ϕ,ϕ2,w2)) = ∫(∂t(u)⋅w + ((q-τ*(u⋅∇(q)))*(perp∘(F)))⋅w - (∇⋅(w))*(g*(h+b) + 0.5*(u⋅u)) + ∂t(h)*ϕ + w2⋅(F-u*h) + ϕ2*(q*h) +  (perp∘(∇(ϕ2)))⋅u - (ϕ2*fn) + ϕ*(∇⋅(F)) + ((cd*(u⋅u)*u)/(h+b))⋅w)dΩ + ∫((g*(h+b) + 0.5*(u⋅u))*(w⋅nΓ))dΓ
+    jac(t,(u,h,q,F),(du,dh,dq,dF),(w,ϕ,ϕ2,w2)) = ∫(((dq - τ*(u⋅∇(dq) + du⋅∇(q)))*(perp∘(F)))⋅w + ((q-τ*(u⋅∇(q)))*(perp∘(dF)))⋅w - (∇⋅(w))*(g*(dh) + (du⋅u)) + w2⋅(dF - du*h - dh*u) + ϕ2*(q*dh) + ϕ2*(dq*h) + du⋅(perp∘(∇(ϕ2))) + ϕ*(∇⋅(dF)) + ((cd*2*(du⋅u)*u)/(h+b))⋅w + ((cd*(u⋅u)*du)/(h+b))⋅w)dΩ+ ∫((g*(h+b) + 0.5*(du⋅u))*(w⋅nΓ))dΓ
     jac_t(t,(u,h),(dut,dht),(w,ϕ)) = ∫(dut⋅w + dht*ϕ)dΩ
 
 
-    assem = SparseMatrixAssembler(sparse_matrix_type,Vector{Float64},X,Y)
     op = TransientFEOperator(res,jac,jac_t,X,Y)
     nls = NLSolver(show_trace=true,linesearch=BackTracking())
-    Tend = 10
-    ode_solver = ThetaMethod(nls,0.1,0.5)
-    x = solve(ode_solver,op,uhn,0.0,Tend)
-    dir = "swe-solver/1d-topo-output_zero"
+    ode_solver = ThetaMethod(nls,dt,0.5)
+    x = solve(ode_solver,op,uhn,T0,Tend)
+    dir = "swe-solver/nonlinear_drop_friction"
     if isdir(dir)
-        output_file = paraview_collection(joinpath(dir,"1d-topo-output"))do pvd
-            #pvd[0.0] = createvtk(Ω,joinpath(dir,"1d-topo0.0.vtu"),cellfields=["u"=>un,"h"=>(hn+b),"b"=>b])
+        output_file = paraview_collection(joinpath(dir,"nonlinear_topo"))do pvd
+            pvd[0.0] = createvtk(Ω,joinpath(dir,"nonlinear_topo0.0.vtu"),cellfields=["u"=>un,"h"=>(hn+b),"b"=>b])
             for (x,t) in x
                 u,h,F = x
-                pvd[t] = createvtk(Ω,joinpath(dir,"1d-topo$t.vtu"),cellfields=["u"=>u,"h"=>(h+b),"b"=>b])
+                pvd[t] = createvtk(Ω,joinpath(dir,"nonlinear_topo$t.vtu"),cellfields=["u"=>u,"h"=>(h+b),"b"=>b])
                 println("done $t/$Tend")
             end
         end
     else
         mkdir(dir)
-        output_file = paraview_collection(joinpath(dir,"1d-topo-output")) do pvd
-            #pvd[0.0] = createvtk(Ω,joinpath(dir,"1d-topo0.0.vtu"),cellfields=["u"=>un,"h"=>(hn+b),"b"=>b])
+        output_file = paraview_collection(joinpath(dir,"nonlinear_topo")) do pvd
+            pvd[0.0] = createvtk(Ω,joinpath(dir,"nonlinear_topo0.0.vtu"),cellfields=["u"=>un,"h"=>(hn+b),"b"=>b])
             for (x,t) in x
                 u,h,F = x
-                pvd[t] = createvtk(Ω,joinpath(dir,"1d-topo$t.vtu"),cellfields=["u"=>u,"h"=>(h+b),"b"=>b])
+                pvd[t] = createvtk(Ω,joinpath(dir,"nonlinear_topo$t.vtu"),cellfields=["u"=>u,"h"=>(h+b),"b"=>b])
                 println("done $t/$Tend")
             end
         end
@@ -193,7 +208,7 @@ function Shallow_water_theta_newton(
 end
 
 function h₀((x,y))
-    h = -topography((x,y)) +  2  + 0.5*exp(-10*(x-5)^2 -10*(y-5)^2)
+    h = -topography((x,y)) +  2  + 1*exp(-1*(x-5)^2 -1*(y-2.5)^2)
     h
 end
 
