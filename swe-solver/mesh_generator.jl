ca880e7579f41e7df1937db5e8e58d40d731453f
victoriayuechen <y.chen-72@student.tudelft.nl>
2022-12-20 19:33:30 +0100

    Remove error from loss of precision

--- swe-solver/mesh_generator.jl
+++ swe-solver/mesh_generator.jl
@@ -14,29 +14,29 @@ Parameters:
     lc: The size of the triangles, default is 1e-2. 
 Creates a <filename>.msh in the meshes folder.
 """
-function generate_rectangle_mesh(Lx::Float64, Ly::Float64, filename::String, modelname::String, lc::Float64, periodic::Bool)
+function generate_rectangle_mesh(Lx::Float32, Ly::Float32, filename::String, modelname::String, lc::Float32, periodic::Bool)
     # Initialise mesh generator 
     gmsh.initialize()
     gmsh.model.add(modelname)
 
     # Add the points 
-    gmsh.model.geo.addPoint(0, 0, 0, lc, 1)
-    gmsh.model.geo.addPoint(Lx, 0, 0, lc, 2)
-    gmsh.model.geo.addPoint(Lx, Ly, 0, lc, 3)
-    gmsh.model.geo.addPoint(0, Ly, 0, lc, 4)
+    p1 = gmsh.model.geo.addPoint(0, 0, 0, lc, 1)
+    p2 = gmsh.model.geo.addPoint(Lx, 0, 0, lc, 2)
+    p3 = gmsh.model.geo.addPoint(Lx, Ly, 0, lc, 3)
+    p4 = gmsh.model.geo.addPoint(0, Ly, 0, lc, 4)
 
     # Add the lines 
     gmsh.model.geo.addLine(1, 2, 1)
-    gmsh.model.geo.addLine(2, 3, 2)
-    gmsh.model.geo.addLine(3, 4, 3)
-    gmsh.model.geo.addLine(4, 1, 4)
+    gmsh.model.geo.addLine(3, 2, 2)
+    gmsh.model.geo.addLine(3, p4, 3)
+    gmsh.model.geo.addLine(4, 1, p4)
 
-    gmsh.model.geo.addCurveLoop([1, 2, 3, 4], 1)
+    gmsh.model.geo.addCurveLoop([4, 1, -2, 3], 1)
     gmsh.model.geo.addPlaneSurface([1], 1)
 
     gmsh.model.geo.synchronize()
     if periodic
-        gmsh.model.mesh.setPeriodic(1, [3], [1], [1, 0, 0, 0, 0, 1, Ly, 0, 0, 0, 1, 0, 0, 0, 0, 1])
+        gmsh.model.mesh.setPeriodic(1, [1], [3], [1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1])
     end
 
     gmsh.model.addPhysicalGroup(0, [1, 2], 1, "bottom")
@@ -48,8 +48,8 @@ function generate_rectangle_mesh(Lx::Float64, Ly::Float64, filename::String, mod
 
     gmsh.model.addPhysicalGroup(2, [1], 1, "interior")
     gmsh.model.mesh.generate(2)
-       
     gmsh.write(filename)
+
     gmsh.finalize()
 end 
 
--- swe-solver/tests/testmesh.jl
+++ swe-solver/tests/testmesh.jl
@@ -3,6 +3,6 @@ include("../mesh_generator.jl")
 using .MyMeshGenerator
 
 
-generate_rectangle_mesh(10.0, 10.0, "10x10periodic2.msh", "rectangle", 0.01,true)
+generate_rectangle_mesh(2.0, 2.0, "10x10periodic2.msh", "rectangle", 0.1, true)
 
 # Small test for whether the mesh generator works 
