62add19926ef2789c3fde27a0fea9fb1e4c561bd
Bamischijf_275 <degrootjurgen39@gmail.com>
2023-01-04 12:12:08 +0100

    linear_SWE_test working, nomenclature changed

--- swe-solver/linear_SWE.jl
+++ swe-solver/linear_SWE.jl
@@ -1,6 +1,4 @@
-using Pkg
-Pkg.activate(".")
-
+module Linear_SWE_solver
 using Gridap
 using WriteVTK
 using LineSearches: BackTracking
@@ -8,46 +6,30 @@ using GridapGmsh
 # include("mesh_generator.jl")
 # using .MyMeshGenerator
 
-
+export run_linear_SWE
 
 function perp(u)
     p = VectorValue(-u[2],u[1])
     p
 end
 
-function uh(u₀,h₀,X,Y,dΩ)
-    a((u,h),(w,ϕ)) = ∫(w⋅u +ϕ*h)dΩ
-    b((w,ϕ)) = ∫(w⋅u₀ + ϕ*h₀)dΩ
+function uζ(u₀,ζ₀,X,Y,dΩ)
+    a((u,ζ),(w,ϕ)) = ∫(w⋅u +ϕ*ζ)dΩ
+    b((w,ϕ)) = ∫(w⋅u₀ + ϕ*ζ₀)dΩ
     solve(AffineFEOperator(a,b,X,Y))
 end
 
 
-function linear_SWE(order,degree,h₀,u₀)
+function run_linear_SWE(order,degree,ζ₀,u₀,model)
 
     #Parameters
-    B = 100 #Channel width
-    L = 100 #Channel Length
     latitude = 52
     η = 7.29e-5
     f = 2*η*sin(latitude*(π/180))
     g = 9.81
     H = 0.5 #Constant layer depth at rest
 
-
-    #Make model
-    domain = (0,B,0,L)
-    partition = (50,50)
     # Generate the model
-    # model = GmshDiscreteModel("swe-solver/meshes/100x100periodic2.msh")
-    model = CartesianDiscreteModel(domain,partition;isperiodic=(false,true))
-
-    # #Make labels
-    labels = get_face_labeling(model)
-    add_tag_from_tags!(labels,"bottom",[1,2,5])
-    add_tag_from_tags!(labels,"left",[7])
-    add_tag_from_tags!(labels,"right",[8])
-    add_tag_from_tags!(labels,"top",[3,4,6])
-    add_tag_from_tags!(labels,"inside",[9])
     DC = ["left","right"]
     dir = "swe-solver/output_linear_swe"
     Ω = Triangulation(model)
@@ -79,54 +61,42 @@ function linear_SWE(order,degree,h₀,u₀)
     un = solve(AffineFEOperator(a1,l1,U,V))
 
     a2(u,v) = ∫(v*u)dΩ
-    l2(v) = ∫(v*h₀)dΩ
-    hn = solve(AffineFEOperator(a2,l2,P,Q))
+    l2(v) = ∫(v*ζ₀)dΩ
+    ζn = solve(AffineFEOperator(a2,l2,P,Q))
 
-    uhn = uh(un,hn,X,Y,dΩ)
-    un,hn = uhn
+    uζn = uζ(un,ζn,X,Y,dΩ)
+    un,ζn = uζn
     #forcefunc(t) = VectorValue(0.0,0.0*0.5*cos(π*(1/5)*t))
 
-    res(t,(u,h),(w,ϕ)) = ∫(∂t(u)⋅w + w⋅(f*(perp∘(u))) - (∇⋅(w))*g*h + ∂t(h)*ϕ + ϕ*H*(∇⋅(u)))dΩ
-    jac(t,(u,h),(du,dh),(w,ϕ)) = ∫(w⋅(f*(perp∘(du))) - (∇⋅(w))*g*dh + ϕ*(H*(∇⋅(du))))dΩ
-    jac_t(t,(u,h),(dut,dht),(w,ϕ)) = ∫(dut⋅w + dht*ϕ)dΩ
+    res(t,(u,ζ),(w,ϕ)) = ∫(∂t(u)⋅w + w⋅(f*(perp∘(u))) - (∇⋅(w))*g*ζ + ∂t(ζ)*ϕ + ϕ*H*(∇⋅(u)))dΩ
+    jac(t,(u,ζ),(du,dζ),(w,ϕ)) = ∫(w⋅(f*(perp∘(du))) - (∇⋅(w))*g*dζ + ϕ*(H*(∇⋅(du))))dΩ
+    jac_t(t,(u,ζ),(dut,dζt),(w,ϕ)) = ∫(dut⋅w + dζt*ϕ)dΩ
 
     op = TransientFEOperator(res,jac,jac_t,X,Y)
     nls = NLSolver(show_trace=true,linesearch=BackTracking())
     Tend = 50
     ode_solver = ThetaMethod(nls,0.5,0.5)
-    x = solve(ode_solver,op,uhn,0.0,Tend)
+    x = solve(ode_solver,op,uζn,0.0,Tend)
 
     if isdir(dir)
         output_file = paraview_collection(joinpath(dir,"linear_topo"))do pvd
-            pvd[0.0] = createvtk(Ω,joinpath(dir,"linear_topo0.0.vtu"),cellfields=["u"=>un,"h"=>(hn)])
+            pvd[0.0] = createvtk(Ω,joinpath(dir,"linear_topo0.0.vtu"),cellfields=["u"=>un,"ζ"=>(ζn)])
             for (x,t) in x
-                u,h = x
-                pvd[t] = createvtk(Ω,joinpath(dir,"linear_topo$t.vtu"),cellfields=["u"=>u,"h"=>(h+H)])
+                u,ζ = x
+                pvd[t] = createvtk(Ω,joinpath(dir,"linear_topo$t.vtu"),cellfields=["u"=>u,"ζ"=>(ζ+H)])
                 println("done $t/$Tend")
             end
         end
     else
         mkdir(dir)
         output_file = paraview_collection(joinpath(dir,"linear_topo")) do pvd
-            pvd[0.0] = createvtk(Ω,joinpath(dir,"linear_topo0.0.vtu"),cellfields=["u"=>un,"h"=>(hn)])
+            pvd[0.0] = createvtk(Ω,joinpath(dir,"linear_topo0.0.vtu"),cellfields=["u"=>un,"ζ"=>(ζn)])
             for (x,t) in x
-                u,h = x
-                pvd[t] = createvtk(Ω,joinpath(dir,"linear_topo$t.vtu"),cellfields=["u"=>u,"h"=>(h+H)])
+                u,ζ = x
+                pvd[t] = createvtk(Ω,joinpath(dir,"linear_topo$t.vtu"),cellfields=["u"=>u,"ζ"=>(ζ+H)])
                 println("done $t/$Tend")
             end
         end
     end
 end
-
-function h₀((x,y))
-    h =0.05*exp(-0.01*(x-25)^2 -0.01*(y-25)^2)
-    h
 end
-
-
-function u₀((x,y))
-    u = VectorValue(0.0,0.0)
-    u
-end
-
-linear_SWE(0,3,h₀,u₀)
