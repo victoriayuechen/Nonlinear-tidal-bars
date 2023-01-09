98933f0e09cbecc6299a654dd1de9dd118c04c0c
rafalberes <rafal3.beres@gmail.com>
2023-01-09 17:20:53 +0100

    Raviart Thomas version

--- swe-solver/linear_SWE_ver.jl
+++ swe-solver/linear_SWE_ver.jl
@@ -27,11 +27,11 @@ function linear_SWE(order,degree)
 
     #Parameters
     B = 1 #Channel width
-    L = 1 #Channel Length
+    L = 5 #Channel Length
     latitude = 52
     η = 7.29e-5
-    #f = 2*η*sin(latitude*(π/180))
-    f=0
+    f = 2*η*sin(latitude*(π/180))
+    #f=0
     g = 9.81
     H = 0.5 #Constant layer depth at rest
     Tend = 50
@@ -43,7 +43,7 @@ function linear_SWE(order,degree)
     domain = (0,B,0,L)
     partition = (20,20)
     # Generate the model
-    model = CartesianDiscreteModel(domain,partition;isperiodic=(false,false))
+    model = CartesianDiscreteModel(domain,partition;isperiodic=(false,true))
 
     #Make labels
     labels = get_face_labeling(model)
@@ -53,7 +53,7 @@ function linear_SWE(order,degree)
     add_tag_from_tags!(labels,"top",[3,4,6])
     add_tag_from_tags!(labels,"inside",[9])
     writevtk(model, "verification/output/model")
-    DC = ["left","right","top","bottom"]
+    DC = ["left","right"]
     dir = "verification/output"
     Ω = Triangulation(model)
     dΩ = Measure(Ω,degree)
@@ -73,16 +73,32 @@ function linear_SWE(order,degree)
     # ug(x, t::Real) = VectorValue(sin((π * t)/(2 * Tend)), sin((π * t)/(2 * Tend)))
     # ug(t::Real) = x -> ug(x,t)
 
-    u(x,t::Real) = VectorValue(sin((π * t)/(2 * Tend)) * sin((π * x[1])/(B1)) * sin((π * x[2])/(L)),
-                                 sin((π * t)/(2 * Tend)) * sin((π * x[1])/(B)) * sin((π * x[2])/(L)))
+    # u(x,t::Real) = VectorValue(sin((π * t)/(2 * Tend)) * sin((π * x[1])/(B1)) * sin((π * x[2])/(L)),
+    #                              sin((π * t)/(2 * Tend)) * sin((π * x[1])/(B)) * sin((π * x[2])/(L)))
+    # u(t::Real) = x -> u(x,t)
+
+    # h(x,t::Real) = sin((π * t)/(2 * Tend)) * sin((π * x[1])/(B)) * sin((π * x[2])/(L))
+    # h(t::Real) = x -> h(x,t)
+
+    A = (π/B)
+    B = (π/L)
+    C = (π/(2 * Tend))
+
+    D = (π/B)
+    E = (π/(7 * L))
+    F = A
+    G = B
+
+    yref = L/2
+
+    u(x,t::Real) = VectorValue(sin(A*x[1])*sin(B*x[2])*sin(C*t),
+                                sin(A*x[1])*cos(E*(x[2]-yref))*sin(C*t))
     u(t::Real) = x -> u(x,t)
 
-    h(x,t::Real) = sin((π * t)/(2 * Tend)) * sin((π * x[1])/(B)) * sin((π * x[2])/(L))
+    h(x,t::Real) = sin(F*x[1])*sin(G*x[2])*sin(C*t)
     h(t::Real) = x -> h(x,t)
 
-    # ug(x, t::Real) = VectorValue(sin((π * t)/(2 * Tend)), sin((π * t)/(2 * Tend)))
-    # ug(t::Real) = x -> ug(x,t)
-
+    
     #corresponding force functions
     # f_u(x,t::Real) = VectorValue((π/(2 * Tend)) * cos((π * t)/(2 * Tend)) * (x[1] + x[2]) - f * sin((π * t)/(2 * Tend)) * (x[1] + x[2]) + g * sin((π * t)/(2 * Tend)),
     # (π/(2 * Tend)) * cos((π * t)/(2 * Tend)) * (x[1] + x[2]) + f * sin((π * t)/(2 * Tend)) * (x[1] + x[2]) + g * sin((π * t)/(2 * Tend)))
@@ -91,61 +107,43 @@ function linear_SWE(order,degree)
     # f_h(x,t::Real) = (π/(2 * Tend)) * cos((π * t)/(2 * Tend)) * (x[1] + x[2]) + H * 2 * sin((π * t)/(2 * Tend))
     # f_h(t::Real) = x -> f_h(x,t)
 
-    f_u(x,t::Real) = VectorValue(((π * g)/B) * sin((π * t)/(2 * Tend)) * cos((π * x[1])/(B)) * sin((π * x[2])/(L)) + 
-                                (π/(2 * Tend)) * cos((π * t)/(2 * Tend)) * sin((π * x[1])/(B1)) * sin((π * x[2])/(L)),
-                                ((π * g)/L) * sin((π * t)/(2 * Tend)) * sin((π * x[1])/(B)) * cos((π * x[2])/(L)) + 
-                                (π/(2 * Tend)) * cos((π * t)/(2 * Tend)) * sin((π * x[1])/(B1)) * sin((π * x[2])/(L)))
+    # f_u(x,t::Real) = VectorValue(((π * g)/B) * sin((π * t)/(2 * Tend)) * cos((π * x[1])/(B)) * sin((π * x[2])/(L)) + 
+    #                             (π/(2 * Tend)) * cos((π * t)/(2 * Tend)) * sin((π * x[1])/(B1)) * sin((π * x[2])/(L)),
+    #                             ((π * g)/L) * sin((π * t)/(2 * Tend)) * sin((π * x[1])/(B)) * cos((π * x[2])/(L)) + 
+    #                             (π/(2 * Tend)) * cos((π * t)/(2 * Tend)) * sin((π * x[1])/(B1)) * sin((π * x[2])/(L)))
+    # f_u(t::Real) = x -> f_u(x,t)
+
+    # f_h(x,t::Real) = H * ((π/B1) * sin((π * t)/(2 * Tend)) * cos((π * x[1])/(B1)) * sin((π * x[2])/(L)) + 
+    #                         (π/L) * sin((π * t)/(2 * Tend)) * sin((π * x[1])/(B1)) * cos((π * x[2])/(L))) +
+    #                         (π/(2 * Tend)) * cos((π * t)/(2 * Tend)) * sin((π * x[1])/(B)) * sin((π * x[2])/(L))
+    # f_h(t::Real) = x -> f_h(x,t)
+
+    f_u(x,t::Real) = VectorValue(F*g*cos(F*x[1])*sin(G*x[2])*sin(C*t)+C*sin(A*x[1])*sin(B*x[2])*cos(C*t),
+                                G*g*sin(F*x[1])*cos(G*x[2])*sin(C*t)+C*sin(A*x[1])*cos(E*(x[2]-yref))*cos(C*t))
     f_u(t::Real) = x -> f_u(x,t)
 
-    f_h(x,t::Real) = H * ((π/B1) * sin((π * t)/(2 * Tend)) * cos((π * x[1])/(B1)) * sin((π * x[2])/(L)) + 
-                            (π/L) * sin((π * t)/(2 * Tend)) * sin((π * x[1])/(B1)) * cos((π * x[2])/(L))) +
-                            (π/(2 * Tend)) * cos((π * t)/(2 * Tend)) * sin((π * x[1])/(B)) * sin((π * x[2])/(L))
+    f_h(x,t::Real) =  H*(A*cos(A*x[1])*sin(B*x[2])*sin(C*t)-E*sin(A*x[1])*sin(E*(x[2]-yref))*sin(C*t))+C*sin(F*x[1])*sin(G*x[2])*cos(C*t)
     f_h(t::Real) = x -> f_h(x,t)
+
     #Make reference spaces
 
-    reffe_rt = ReferenceFE(raviart_thomas,Float64, order)
-    V = TestFESpace(model,reffe_rt,conformity=:HDiv,dirichlet_tags=DC)
-    udc(x,t::Real) = VectorValue(1.0, 0.0)
+    reffe_rt = ReferenceFE(raviart_thomas, Float64, order)
+    V = TestFESpace(model,reffe_rt,conformity=:HDiv,dirichlet_tags=DC)#, dirichlet_masks=[(false, true), (false, true)])
+    udc(x,t::Real) = VectorValue(0.0, 0.0)
     udc(t::Real) = x -> udc(x,t)
-    U = TransientTrialFESpace(V, [u, u, u, u])
+    U = TransientTrialFESpace(V, [udc, udc])
 
     reffe_lg = ReferenceFE(lagrangian,Float64,order)
-    Q = TestFESpace(model,reffe_lg,conformity=:H1, dirichlet_tags=DC)
+    Q = TestFESpace(model,reffe_lg,conformity=:L2, dirichlet_tags=DC)
     hdc(x,t::Real) = 0.0
     hdc(t::Real) = x -> hdc(x,t)
-    P = TransientTrialFESpace(Q, [hdc, hdc, hdc, hdc])
+    P = TransientTrialFESpace(Q)
 
     Y = MultiFieldFESpace([V,Q])
     X = TransientMultiFieldFESpace([U,P])
 
-    # a1(u,v) = ∫(v⋅u)dΩ
-    # l1(v) = ∫(v⋅u₀)dΩ
-    # un = solve(TransientFEOperator(a1,l1,U,V))
 
-    # a2(u,v) = ∫(v*u)dΩ
-    # l2(v) = ∫(v*h₀)dΩ
-    # hn = solve(TransientFEOperator(a2,l2,P,Q))
-
-    # #Lu_m(x,t::Real) = fu1_res(x, t)
-    # Lu_m(x,t::Real) = VectorValue(π*cos(π*t)*sin(π*x[1])*sin(π*x[2])*sin(π*t/2) + sin(π*x[1])^2*sin(π*x[2])*π*cos(π*t/2)/2 + sin(π*x[1])*sin(π*x[2])*π*cos(π*t/2)*cos(π*x[2])/2 + sin(π*x[1])*sin(π*x[2])*π*cos(π*t/2)*sin(π*t)/2 - f*sin(π*x[1])*sin(π*x[2])*sin(π*t/2)*cos(π*x[1]) - f*sin(π*x[1])*sin(π*x[2])^2*sin(π*t/2) - f*sin(π*x[1])*sin(π*x[2])*sin(π*t/2)*sin(π*t) + g*π*cos(π*x[1]), π*cos(π*t)*sin(π*x[1])*sin(π*x[2])*sin(π*t/2) + sin(π*x[1])*sin(π*x[2])*π*cos(π*t/2)*cos(π*x[1])/2 + sin(π*x[1])*sin(π*x[2])^2*π*cos(π*t/2)/2 + sin(π*x[1])*sin(π*x[2])*π*cos(π*t/2)*sin(π*t)/2 + f*sin(π*x[1])^2*sin(π*x[2])*sin(π*t/2) + f*sin(π*x[1])*sin(π*x[2])*sin(π*t/2)*cos(π*x[2]) + f*sin(π*x[1])*sin(π*x[2])*sin(π*t/2)*sin(π*t) + g*π*cos(π*x[2]))
-    # Lu_m(t::Real) = x -> Lu_m(x, t)
-
-    # #Lh_m(x, t::Real) = fh_res(x[1], x[2], t)
-    # Lh_m(x, t::Real) = π*cos(π*t) + 2*H*π*cos(π*x[1])*sin(π*x[1])*sin(π*x[2])*sin(π*t/2) + H*π*cos(π*x[1])*sin(π*x[2])*sin(π*t/2)*cos(π*x[2]) + H*π*cos(π*x[1])*sin(π*x[2])*sin(π*t/2)*sin(π*t) + 2*H*π*cos(π*x[2])*sin(π*x[1])*sin(π*x[2])*sin(π*t/2) + H*sin(π*x[1])*π*cos(π*x[2])*sin(π*t/2)*cos(π*x[1]) + H*sin(π*x[1])*π*cos(π*x[2])*sin(π*t/2)*sin(π*t)
-    # Lh_m(t::Real) = x -> Lh_m(x, t)
-
-    # u_m(x, t::Real) = VectorValue(sin(π*x[1])^2*sin(π*x[2])*sin(π*t/2) + cos(π*x[2])*sin(π*x[1])*sin(π*x[2])*sin(π*t/2) + sin(π*x[1])*sin(π*x[2])*sin(π*t/2)*sin(π*t), cos(π*x[1])*sin(π*x[1])*sin(π*x[2])*sin(π*t/2) + sin(π*x[1])*sin(π*x[2])^2*sin(π*t/2) + sin(π*x[1])*sin(π*x[2])*sin(π*t/2)*sin(π*t))
-    # u_m(t::Real) = x -> u_m(x, t)
-
-    # h_m(x, t::Real) = 0.5 + sin(π*x[1]) #+ sin(π*x[2]) + sin(π*t)
-    # h_m(t::Real) = x -> h_m(x, t)
-
-    # uhn = uh(un,hn,X,Y,dΩ)
-    # un,hn = uhn
-    #forcefunc(t) = VectorValue(0.0,0.0*0.5*cos(π*(1/5)*t))
-
-    #res(t,(u,h),(w,ϕ)) = ∫(∂t(u)⋅w + w⋅(f*(perp∘(u))) - (∇⋅(w))*g*h + ∂t(h)*ϕ + ϕ*H*(∇⋅(u)) - f_u(t)⋅w - f_h(t)*ϕ)dΩ #- Lu_m(t)⋅w - Lh_m(t)*ϕ
-    res(t,(u,h),(w,ϕ)) = ∫(∂t(u)⋅w + w⋅(f*(perp∘(u))) + (∇(h) ⋅ w)*g + ∂t(h)*ϕ - (∇(ϕ)⋅(u*H)) - f_u(t)⋅w - f_h(t)*ϕ)dΩ # + f_u(t)⋅w + f_h(t)*ϕ
+    res(t,(u,h),(w,ϕ)) = ∫(∂t(u)⋅w + w⋅(f*(perp∘(u))) + (∇⋅(w))*g*h + ∂t(h)*ϕ + ϕ*H*(∇⋅(u)) - f_u(t)⋅w - f_h(t)*ϕ)dΩ # + f_u(t)⋅w + f_h(t)*ϕ
     #jac(t,(u,h),(du,dh),(w,ϕ)) = ∫(w⋅(f*(perp∘(du))) - (∇⋅(w))*g*dh + ϕ*(H*(∇⋅(du))))dΩ
     #jac_t(t,(u,h),(dut,dht),(w,ϕ)) = ∫(dut⋅w + dht*ϕ)dΩ
 
@@ -216,4 +214,4 @@ end
 #     u_man = VectorValue((sin(π*x) + cos(π*y) + sin(π*t))*sin(π*x)*sin(π*y)*(-exp(t) + 1))
 
 # end
-linear_SWE(1,2)
+linear_SWE(0,3)
