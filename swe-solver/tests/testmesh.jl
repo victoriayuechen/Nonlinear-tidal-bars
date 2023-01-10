8b73ce9d1490f30bf77bdcc45f239f7716b5faff
Bamischijf275 <degrootjurgen39@gmail.com>
2023-01-10 11:16:05 +0100

    merge

--- swe-solver/tests/testmesh.jl
+++ swe-solver/tests/testmesh.jl
@@ -3,6 +3,9 @@ includet("../mesh_generator.jl")
 using .MyMeshGenerator
 
 
-generate_rectangle_mesh(Float32(100), Float32(100), "100x100periodic2.msh", "rectangle", Float32(4),true)
+# generate_rectangle_mesh(Float32(100), Float32(100), "100x100periodic2.msh", "rectangle", Float32(4),true)
+
+generate_arc_mesh(Float32(100), Float32(100), "arc-larger.msh", "arc", Float32(1) ,true)
+
 
 # Small test for whether the mesh generator works 
