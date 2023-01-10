1df19097cc5d25caeea12c05a42cd41e16747314
Bamischijf275 <degrootjurgen39@gmail.com>
2023-01-10 11:03:04 +0100

    misc changes that make easier to read

--- swe-solver/linear_SWE.jl
+++ swe-solver/linear_SWE.jl
@@ -1,6 +1,3 @@
-using Pkg
-Pkg.activate(".")
-
 using Gridap
 using WriteVTK
 using LineSearches: BackTracking
@@ -8,46 +5,31 @@ using GridapGmsh
 # include("mesh_generator.jl")
 # using .MyMeshGenerator
 
-
+export run_linear_SWE
 
 function perp(u)
     p = VectorValue(-u[2],u[1])
     p
 end
 
-function uh(u₀,h₀,X,Y,dΩ)
-    a((u,h),(w,ϕ)) = ∫(w⋅u +ϕ*h)dΩ
-    b((w,ϕ)) = ∫(w⋅u₀ + ϕ*h₀)dΩ
+function uζ(u₀,ζ₀,X,Y,dΩ)
+    a((u,ζ),(w,ϕ)) = ∫(w⋅u +ϕ*ζ)dΩ
+    b((w,ϕ)) = ∫(w⋅u₀ + ϕ*ζ₀)dΩ
     solve(AffineFEOperator(a,b,X,Y))
 end
 
 
-function linear_SWE(order,degree,h₀,u₀)
+function run_linear_SWE(order,degree,ζ₀,u₀,forcefunc)
 
-    #Parameters
-    B = 100 #Channel width
-    L = 100 #Channel Length
+    #Parameters 
     latitude = 52
     η = 7.29e-5
     f = 2*η*sin(latitude*(π/180))
     g = 9.81
     H = 0.5 #Constant layer depth at rest
 
-
-    #Make model
-    domain = (0,B,0,L)
-    partition = (50,50)
     # Generate the model
-    # model = GmshDiscreteModel("swe-solver/meshes/100x100periodic2.msh")
-    model = CartesianDiscreteModel(domain,partition;isperiodic=(false,true))
-
-    # #Make labels
-    labels = get_face_labeling(model)
-    add_tag_from_tags!(labels,"bottom",[1,2,5])
-    add_tag_from_tags!(labels,"left",[7])
-    add_tag_from_tags!(labels,"right",[8])
-    add_tag_from_tags!(labels,"top",[3,4,6])
-    add_tag_from_tags!(labels,"inside",[9])
+    model = GmshDiscreteModel("swe-solver/meshes/arc-larger.msh")
     DC = ["left","right"]
     dir = "swe-solver/output_linear_swe"
     Ω = Triangulation(model)
@@ -57,69 +39,61 @@ function linear_SWE(order,degree,h₀,u₀)
     nΓ = get_normal_vector(Γ)
     dΓ = Measure(Γ,degree)
 
-
-
     #Make reference spaces
-
     reffe_rt = ReferenceFE(raviart_thomas,Float64,order)#
-    V = TestFESpace(model,reffe_rt,conformity=:HDiv,dirichlet_tags=DC)#
+    V = TestFESpace(model,reffe_rt,dirichlet_tags=DC,conformity=:HDiv)#
     udc(x,t::Real) = VectorValue(0.0,0.0)
-    udc = x -> udc(x,t)
-    U = TransientTrialFESpace(V)
+    udc(t::Real) = x -> udc(x,t)
+    U = TransientTrialFESpace(V,[udc,udc])
 
     reffe_lg = ReferenceFE(lagrangian,Float64,order)
     Q = TestFESpace(model,reffe_lg,conformity=:L2)
     P = TransientTrialFESpace(Q)
 
     Y = MultiFieldFESpace([V,Q])
-    X = MultiFieldFESpace([U,P])
+    X = TransientMultiFieldFESpace([U,P])
 
-    a1(u,v) = ∫(v⋅u)dΩ
-    l1(v) = ∫(v⋅u₀)dΩ
-    un = solve(AffineFEOperator(a1,l1,U,V))
 
-    a2(u,v) = ∫(v*u)dΩ
-    l2(v) = ∫(v*h₀)dΩ
-    hn = solve(AffineFEOperator(a2,l2,P,Q))
+    x0 = interpolate_everywhere([u₀,ζ₀],X(0.0))
 
-    uhn = uh(un,hn,X,Y,dΩ)
-    un,hn = uhn
-    #forcefunc(t) = VectorValue(0.0,0.0*0.5*cos(π*(1/5)*t))
 
-    res(t,(u,h),(w,ϕ)) = ∫(∂t(u)⋅w + w⋅(f*(perp∘(u))) - (∇⋅(w))*g*h + ∂t(h)*ϕ + ϕ*H*(∇⋅(u)))dΩ
-    jac(t,(u,h),(du,dh),(w,ϕ)) = ∫(w⋅(f*(perp∘(du))) - (∇⋅(w))*g*dh + ϕ*(H*(∇⋅(du))))dΩ
-    jac_t(t,(u,h),(dut,dht),(w,ϕ)) = ∫(dut⋅w + dht*ϕ)dΩ
+    # uζn = uζ(un,ζn,X,Y,dΩ)
+    # un,ζn = uζn
+    
+
+    forcefunc_x(t) = x -> forcefunc(x,t)
+
+    res(t,(u,ζ),(w,ϕ)) = ∫(∂t(u)⋅w + w⋅(f*(perp∘(u))) - (∇⋅(w))*g*ζ + ∂t(ζ)*ϕ + ϕ*H*(∇⋅(u)) - forcefunc_x(t)⋅w)dΩ
+    jac(t,(u,ζ),(du,dζ),(w,ϕ)) = ∫(w⋅(f*(perp∘(du))) - (∇⋅(w))*g*dζ + ϕ*(H*(∇⋅(du))))dΩ
+    jac_t(t,(u,ζ),(dut,dζt),(w,ϕ)) = ∫(dut⋅w + dζt*ϕ)dΩ
 
     op = TransientFEOperator(res,jac,jac_t,X,Y)
     nls = NLSolver(show_trace=true,linesearch=BackTracking())
     Tend = 50
     ode_solver = ThetaMethod(nls,0.5,0.5)
-    x = solve(ode_solver,op,uhn,0.0,Tend)
+    x = solve(ode_solver,op,x0,0.0,Tend)
 
     if isdir(dir)
         output_file = paraview_collection(joinpath(dir,"linear_topo"))do pvd
-            pvd[0.0] = createvtk(Ω,joinpath(dir,"linear_topo0.0.vtu"),cellfields=["u"=>un,"h"=>(hn)])
             for (x,t) in x
-                u,h = x
-                pvd[t] = createvtk(Ω,joinpath(dir,"linear_topo$t.vtu"),cellfields=["u"=>u,"h"=>(h+H)])
+                u,ζ = x
+                pvd[t] = createvtk(Ω,joinpath(dir,"linear_topo$t.vtu"),cellfields=["u"=>u,"ζ"=>(ζ+H)])
                 println("done $t/$Tend")
             end
         end
     else
         mkdir(dir)
         output_file = paraview_collection(joinpath(dir,"linear_topo")) do pvd
-            pvd[0.0] = createvtk(Ω,joinpath(dir,"linear_topo0.0.vtu"),cellfields=["u"=>un,"h"=>(hn)])
             for (x,t) in x
-                u,h = x
-                pvd[t] = createvtk(Ω,joinpath(dir,"linear_topo$t.vtu"),cellfields=["u"=>u,"h"=>(h+H)])
+                u,ζ = x
+                pvd[t] = createvtk(Ω,joinpath(dir,"linear_topo$t.vtu"),cellfields=["u"=>u,"ζ"=>(ζ+H)])
                 println("done $t/$Tend")
             end
         end
     end
 end
-
-function h₀((x,y))
-    h =0.05*exp(-0.01*(x-25)^2 -0.01*(y-25)^2)
+function ζ₀((x,y))
+    h =0.01*exp(-0.1*(x-150)^2 -0.1*(y-30)^2)
     h
 end
 
@@ -129,4 +103,13 @@ function u₀((x,y))
     u
 end
 
-linear_SWE(0,3,h₀,u₀)
+function forcefunc((x,y),t)
+    f = VectorValue(0.0,0.0*0.5*cos(π*(1/5)*t))
+    f
+end
+
+order = 1
+degree = 4
+
+
+run_linear_SWE(order,degree,ζ₀,u₀,forcefunc)
