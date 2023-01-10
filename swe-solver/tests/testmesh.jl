dadf2a2a43184ee388c1f8106d049bbc99cd7c81
victoriayuechen <y.chen-72@student.tudelft.nl>
2023-01-10 10:32:22 +0100

    Add script for mesh generation

--- swe-solver/tests/testmesh.jl
+++ swe-solver/tests/testmesh.jl
@@ -5,7 +5,7 @@ using .MyMeshGenerator
 
 # generate_rectangle_mesh(Float32(100), Float32(100), "100x100periodic2.msh", "rectangle", Float32(4),true)
 
-generate_arc_mesh(Float32(10), Float32(20), "arc-smaller.msh", "arc", Float32(0.1) ,true)
+generate_arc_mesh(Float32(100), Float32(100), "arc-larger.msh", "arc", Float32(1) ,true)
 
 
 # Small test for whether the mesh generator works 
