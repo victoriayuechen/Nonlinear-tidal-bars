8f64d2c19253bff26e7847edadf4eb9a7056a1c2
victoriayuechen <y.chen-72@student.tudelft.nl>
2023-01-10 14:13:29 +0100

    Add script for generating canal mesh

--- swe-solver/tests/testmesh.jl
+++ swe-solver/tests/testmesh.jl
@@ -3,9 +3,10 @@ includet("../mesh_generator.jl")
 using .MyMeshGenerator
 
 
-# generate_rectangle_mesh(Float32(100), Float32(100), "100x100periodic2.msh", "rectangle", Float32(4),true)
+# generate_rectangle_mesh(Float32(10), Float32(10), "100x100periodic2.msh", "rectangle", Float32(1),true)
 
-generate_arc_mesh(Float32(100), Float32(100), "arc-larger.msh", "arc", Float32(1) ,true)
+# generate_arc_mesh(Float32(100), Float32(100), "arc-larger.msh", "arc", Float32(1) ,true)
 
+generate_outlet_mesh(Float32(100), Float32(100), "canal.msh", "arc", Float32(1) ,true)
 
 # Small test for whether the mesh generator works 
