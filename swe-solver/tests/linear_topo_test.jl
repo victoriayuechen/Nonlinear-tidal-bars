c5bb0c11b25422c04230cb81011f33bad1e3a4f7
Bamischijf275 <degrootjurgen39@gmail.com>
2023-01-26 15:15:20 +0100

    cleanup

--- swe-solver/tests/linear_topo_test.jl
+++ swe-solver/tests/linear_topo_test.jl
@@ -1,17 +1,18 @@
 
 using Gridap
+includet("../linear_topo_SWE.jl")
+using .MylinearSWETopo
 
-includet("../linear_SWE.jl")
-using .MyLinearSWE
 
 
 function ζ₀((x,y))
     ```
     Function of initial ζ
-    ```
-    h =0.01*exp(-0.1*(x-30)^2 -0.1*(y-50)^2)
+    ``` 	
+    h = 0.01*exp(-0.1*(x-50)^2 -0.1*(y-25)^2)
     h
 end
+
 
 function u₀((x,y))
     ```
@@ -25,8 +26,16 @@ function forcefunc((x,y),t)
     ```
     Forcing function
     ```
-    f = VectorValue(0.0,0.0)
-    f
+    func = VectorValue(0.0,0.0)
+    func
+end
+
+function topography((x,y))
+    ```
+    Function for topography
+    ```
+    b = 0.3*exp(-0.01*(y-50)^2)
+    b
 end
 
 
@@ -37,7 +46,7 @@ degree = 4
 B = 100 #Height of rectangle
 L = 100 #Width of rectangle
 partition = (100,100)
-model = CartesianDiscreteModel((0,L,0,B),partition;isperiodic=(true,false))
+model = CartesianDiscreteModel((0,B,0,L),partition;isperiodic=(true,false))
 #Make labels
 labels = get_face_labeling(model)
 add_tag_from_tags!(labels,"bottom",[1,2,5])
@@ -46,21 +55,18 @@ add_tag_from_tags!(labels,"right",[8])
 add_tag_from_tags!(labels,"top",[3,4,6])
 add_tag_from_tags!(labels,"inside",[9])
 DC = ["bottom","top"]
-linear_dir = "output_swe/linear_SWE"
+linear_dir = "output_swe/linear_SWE_topo"
 if isdir(linear_dir)
     dir = joinpath(linear_dir,"test")
 else
     mkdir(linear_dir)
     dir = joinpath(linear_dir,"test")
 end
-filename = "test"
+filename = "testt"
 H = 0.5
 latitude = 50
 Tend = 20
-dt =  0.1
-################
+dt = 0.1
+#################
 
-
-time_1 = time()
-run_linear_SWE(order,degree,ζ₀,u₀,forcefunc,Tend,dt,model,H,DC,dir,latitude,filename)
-println(time() - time_1)
+linear_SWE_topo(order,degree,ζ₀,u₀,topography,forcefunc,Tend,dt,model,H,DC,dir,latitude,filename)
